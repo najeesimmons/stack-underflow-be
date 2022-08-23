46a46f84627512ede72ec94398e3f903b642eea3
najeesimmons <najee.bryant.simmons@gmail.com>
2022-08-23 16:02:48 -0400

    post model routes built/ server built

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Post } = require("../models");
+const { Post } = require("../models"); 
 
 router.get("/", async (req, res) => {
   try {
