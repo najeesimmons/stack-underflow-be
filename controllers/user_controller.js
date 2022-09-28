fe84563467a49a97bf3782085e159e5f10ec952c
najeesimmons <najee.bryant.simmons@gmail.com>
2022-09-28 15:01:53 -0400

    added function to signup controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -8,9 +8,17 @@ const loginUser = (req, res) => {
 };
 
 const signupUser = (req, res) => {
-  res.json({
-    msg: "signupUser working",
-  });
+    const signupUser = async (req, res) => {
+        const {email, password} = req.body
+      
+        try {
+          const user = await User.signup(email, password)
+      
+          res.status(200).json({email, user})
+        } catch (error) {
+          res.status(400).json({error: error.message})
+        }
+      }
 };
 
 module.exports = {
